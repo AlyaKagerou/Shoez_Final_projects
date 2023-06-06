5de6021b07d0ea4233aacdc90a15f08fbae0dd24
AlyaKagerou <alyazalfa46@gmail.com>
2023-06-06 16:39:07 +0700

    membuat fitur darkmode

--- assets/js/dashboard.js
+++ assets/js/dashboard.js
@@ -34,4 +34,20 @@ data: {
             }
             }
         }
-    });
+});
+
+// Toggle Darkmode
+var element = document.body;
+
+function ubahMode(){
+    element.classList.toggle("dark");
+    if (element.classList.contains('dark')){
+        localStorage.setItem('theme', 'dark');
+    }else{
+        localStorage.setItem('theme','light');
+    }
+}
+var theme = localStorage.getItem("theme");
+if(theme == 'dark'){
+    element.classList.add("dark");
+}
