36b3ca93361d9103ca508f6ab63ffbb41e09a057
AlyaKagerou <alyazalfa46@gmail.com>
2023-06-23 14:27:54 +0700

    mengubah tampilan tabel

--- application/views/barang/index.php
+++ application/views/barang/index.php
@@ -1,39 +1,35 @@
-<h1 class="my-3 title">Data Barang</h1>
+<h1 class="mt-3 title">Data Barang</h1>
 
 <!-- main Content -->
 <div class="col-12 col-lg">
-    <div class="bg-card-table mt-4">
-        <table class="table">
-    <thead>
-        <tr>
-        <th scope="col">#</th>
-        <th scope="col">Kode Barang</th>
-        <th scope="col">Nama Barang</th>
-        <th scope="col">Harga Beli</th>
-        <th scope="col">Harga Jual</th>
-        <th scope="col">Stok</th>
-        <th scope="col">Aksi</th>
-        </tr>
-    </thead>
-    <tbody>
-        <tr>
-        <th scope="row">1</th>
-        <td>Mark</td>
-        <td>Otto</td>
-        <td>@mdo</td>
-        </tr>
-        <tr>
-        <th scope="row">2</th>
-        <td>Jacob</td>
-        <td>Thornton</td>
-        <td>@fat</td>
-        </tr>
-        <tr>
-        <th scope="row">3</th>
-        <td colspan="2">Larry the Bird</td>
-        <td>@twitter</td>
-        </tr>
-    </tbody>
-    </table>
+    <div class="bg-card-table mt-4 p-4">
+        <table class="table table-bordered">
+            <thead class="table-light">
+                <tr>
+                <th scope="col">No.</th>
+                <th scope="col">Kode Barang</th>
+                <th scope="col">Nama Barang</th>
+                <th scope="col">Harga Beli</th>
+                <th scope="col">Harga Jual</th>
+                <th scope="col">Stok</th>
+                <th scope="col">Aksi</th>
+                </tr>
+            </thead>
+            <tbody>
+                <tr>
+                    <th scope="row">1</th>
+                    <td>Mark</td>
+                    <td>Otto</td>
+                    <td>@mdo</td>
+                    <td>@mdo</td>
+                    <td>@mdo</td>
+                    <td>
+                        <a href="" class="btn btn-success"><i class='bx bxs-pencil'></i></a>
+                        <a onclick="return confirm('apakah anda yakin?')" href="" class="btn btn-danger"><i class='bx bxs-trash-alt'></i></a>
+                    </td>
+                </tr>
+            </tbody>
+        </table>
     </div>
 </div>
+<!-- main Content -->
